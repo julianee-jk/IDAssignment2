82676dede41475e1848406208f5bd10e4ca67cb1
julianee-jk <s10202587@connect.np.edu.sg>
2020-12-24 03:00:47 +0800

    Added a Homepage & Kanto Pokedex page

--- js/script.js
+++ js/script.js
@@ -2,26 +2,25 @@ const pokedex = document.getElementById('pokedex');
 
 const fetchPokemon = () => {
     const promises = [];
-    for( let i=1; i <= 150; i++){
+    for (let i = 1; i <= 151; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        promises.push(fetch(url).then( res => res.json()));
+        promises.push(fetch(url).then(res => res.json()));
     };
-        
-        Promise.all(promises).then ( results => { 
-            const pokemon = results.map( data => ({
-                id: data.id,
-                name: data.name,
-                image: data.sprites['front_default'],
-                type: data.types.map((type) => type.type.name).join(', ')
-            }));
-            displayPokemon(pokemon);
-        });
-};
 
+    Promise.all(promises).then(results => {
+        const pokemon = results.map(data => ({
+            id: data.id,
+            name: data.name,
+            image: data.sprites['front_default'],
+            type: data.types.map((type) => type.type.name).join(', ')
+        }));
+        displayPokemon(pokemon);
+    });
+};
 
 const displayPokemon = (pokemon) => {
     console.log(pokemon);
-    const pokemonHTMLString = pokemon.map ( indivPoke => `
+    const pokemonHTMLString = pokemon.map(indivPoke => `
     <li class="poke-card"> 
         <img class="poke-image" src="${indivPoke.image}" alt="${indivPoke.name}" />
         <div class="poke-info">
